deb78786a2c5f441a18a938f0149d9d4dfe1c77b
AwfulAsiful <awful872@gmail.com>
2021-09-02 12:27:42 +0600

    js file aded

--- app.js
+++ app.js
@@ -24,34 +24,44 @@ searchBtn.addEventListener('click', function () {
     /*-------Fetching Data--------*/
     fetch(url)
     .then(res=>res.json())
-    .then(data=> displaySearchResult(data.docs))
+    .then(data=> showData(data));
 });
 
 
 
+const showData = (data) =>{
+  searchCount.innerText=`Total Search Results:${data.numFound}`;
+  
 
-const displaySearchResult = (docs) =>{
-
-
-   searchCount.innerText=`total search results:${docs.length}`
-    /* looping through */
-    docs?.forEach(doc => {
-         
-        const div = document.createElement('div');
-        div.classList.add('w-25', 'd-flex', 'flex-column', 'ms-5','mb-3','px-3','border','border-3','border-primary','flex-wrap')
-        div.innerHTML = `
-       
-        <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" alt="" class=" mx-auto pt-3 w-75">
-        
-        <p class="mt-3">Name: ${doc.text[4]}</p>
-        <p class="mt-3">First published year: ${doc.first_publish_year}</p>
-        <p class="mt-3">Publisher: ${doc.publisher[0]}</p>
-        <p class="mt-3">Author name:${doc.author_name}</p>
-        
-      `
+  /* ------Error Handling--------- */
+  if(data.numFound===0){
+    errorMsg.innerText=`No result Found`;
+    searchResults.innerText=``;
+  }
+  else{
+    errorMsg.innerText=``;
+  }
+  data.docs.forEach(doc=>{
+    const div = document.createElement('div');
+    div.classList.add('w-25', 'd-flex', 'flex-column', 'ms-5','mb-3','px-3','border','border-3','border-primary','flex-wrap')
+    div.innerHTML = `
+   
+    <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" alt="" class=" mx-auto pt-3 w-75">
     
-        searchResults.appendChild(div);
-    });
+    <p class="mt-3">Name: ${doc.text[4]}</p>
+    <p class="mt-3">First published year: ${doc.first_publish_year}</p>
+    <p class="mt-3">Publisher: ${doc.publisher[0]}</p>
+    <p class="mt-3">Author name:${doc.author_name}</p>
+    
+  `
+  
+    searchResults.appendChild(div);
+  })
+  
+
 
 
 } 
+
+
+
